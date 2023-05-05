a4676f808b93eefb31e6d602343dce122dc3a80e
a01177702 <a01177702@tec.mx>
2023-05-05 13:44:23 -0600

    button

---
+++ Atos Learning/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public string Scene;
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(Scene);
+        
+    }
+}
