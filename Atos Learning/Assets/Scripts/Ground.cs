89e243ecd7c189b2182e6b63ebe4e5b9130dee67
ArturoDuCas <a00833516@tec.mx>
2023-05-11 15:51:34 -0600

    Replace tilemap plaftorms for rectangles

--- Atos Learning/Assets/Scripts/Ground.cs
+++ Atos Learning/Assets/Scripts/Ground.cs
@@ -12,15 +12,15 @@ public class Ground : MonoBehaviour
     public float screenRight; 
     public float screenLeft; 
 
-    CompositeCollider2D compositeCollider2D;
+    BoxCollider2D boxCollider2D;
     PlayerMovement player; 
 
     bool didGenerateGround = false; 
 
     private void Awake() {
         player = GameObject.Find("Player").GetComponent<PlayerMovement>();
-        compositeCollider2D = GetComponent<CompositeCollider2D>(); 
-        groundHeight = compositeCollider2D.bounds.max.y + 1.5f;  
+        boxCollider2D = GetComponent<BoxCollider2D>(); 
+        groundHeight = boxCollider2D.bounds.max.y + 1.5f; 
         screenRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x;
         screenLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x;
     }
@@ -33,7 +33,8 @@ public class Ground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log(groundRight); 
+        Debug.Log(groundHeight);
     }
 
     private void FixedUpdate() {
@@ -41,8 +42,7 @@ public class Ground : MonoBehaviour
         pos.x -= player.velocity.x * Time.fixedDeltaTime;
 
     
-
-        groundRight = compositeCollider2D.bounds.max.x;
+        groundRight = boxCollider2D.bounds.max.x;
 
         if(groundRight < screenLeft) {
             Destroy(gameObject);
@@ -61,10 +61,10 @@ public class Ground : MonoBehaviour
 
     void generateGround() {
         GameObject go = Instantiate(gameObject);
-        CompositeCollider2D goCollider = go.GetComponent<CompositeCollider2D>();
+        BoxCollider2D goCollider = go.GetComponent<BoxCollider2D>();
 
         Vector2 pos; 
-        pos.x = screenRight + 5; 
+        pos.x = screenRight - 5; 
         pos.y = transform.position.y;
         go.transform.position = pos;
     }
