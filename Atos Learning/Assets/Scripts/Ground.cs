9582b29d91c375aeac7251391f3caa91c940da93
ArturoDuCas <a00833516@tec.mx>
2023-05-14 20:04:05 -0600

    First version of automatic ground generation

--- Atos Learning/Assets/Scripts/Ground.cs
+++ Atos Learning/Assets/Scripts/Ground.cs
@@ -11,6 +11,7 @@ public class Ground : MonoBehaviour
     public float groundRight; 
     public float screenRight; 
     public float screenLeft; 
+    public float screenBottom; 
 
     BoxCollider2D boxCollider2D;
     PlayerMovement player; 
@@ -22,6 +23,7 @@ public class Ground : MonoBehaviour
         boxCollider2D = GetComponent<BoxCollider2D>(); 
         groundHeight = boxCollider2D.bounds.max.y + 1.5f; 
         screenRight = Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height)).x;
+        screenBottom = Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y;
         screenLeft = Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x;
     }
     // Start is called before the first frame update
@@ -33,18 +35,17 @@ public class Ground : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(groundRight); 
-        Debug.Log(groundHeight);
+        
     }
 
     private void FixedUpdate() {
         Vector2 pos = transform.position;
-        pos.x -= player.velocity.x * Time.fixedDeltaTime;
+        pos.x -= player.velocity.x * Time.fixedDeltaTime; // Move the ground to the left based on the player's velocity
 
     
         groundRight = boxCollider2D.bounds.max.x;
 
-        if(groundRight < screenLeft) {
+        if(groundRight < screenLeft) { // If the ground is off the screen, destroy it
             Destroy(gameObject);
             return; 
         }
@@ -62,10 +63,35 @@ public class Ground : MonoBehaviour
     void generateGround() {
         GameObject go = Instantiate(gameObject);
         BoxCollider2D goCollider = go.GetComponent<BoxCollider2D>();
-
         Vector2 pos; 
-        pos.x = screenRight - 5; 
-        pos.y = transform.position.y;
+
+        float h1 = player.jumpVelocity * player.maxJumpTime;  
+        float t = player.jumpVelocity / -player.gravity;
+        float h2 = player.jumpVelocity * t + (0.5f * (-player.gravity * (t * t)));  // h = v0t + 1/2at^2
+        float maxJumpHeight = h1 + h2; 
+        float maxY = maxJumpHeight * 0.7f; // The 70% of the max jump height the player can perform
+        maxY += groundHeight; // Add the actual ground Height
+        float minY = screenBottom - 2f; // The minimum height the ground can be at 
+        float actualY = Random.Range(minY, maxY); 
+        Debug.Log("Max Y: " + maxY + " Min Y: " + minY + " Actual Y: " + actualY); 
+        pos.y = actualY + goCollider.bounds.extents.y;
+        if (pos.y > 4.5f)
+            pos.y = 4.5f;
+
+        float t1 = t + player.maxJumpTime;
+        float t2 = Mathf.Sqrt((2.0f * (maxY - actualY)) / -player.gravity); 
+        float totalTime = t1 + t2; 
+        float maxX = totalTime * player.velocity.x; 
+        maxX *= 0.7f;
+        maxX += groundRight;
+        float minX = screenRight + 2;
+        float actualX = Random.Range(minX, maxX);
+
+        pos.x = actualX + goCollider.bounds.extents.x; 
+
         go.transform.position = pos;
+        
+        Ground goGround = go.GetComponent<Ground>();
+        goGround.groundHeight = pos.y + (goCollider.bounds.extents.y / 1.5f);
     }
 }
