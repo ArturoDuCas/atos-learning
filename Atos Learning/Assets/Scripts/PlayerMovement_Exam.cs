0030d190ee5859c37fe1b9bcb5e78fc2357d5c9d
a01177702 <a01177702@tec.mx>
2023-05-25 18:44:20 -0600

    ExamScenePlayerMovement

--- Atos Learning/Assets/Scripts/PlayerMovement_Exam.cs
+++ Atos Learning/Assets/Scripts/PlayerMovement_Exam.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class PlayerMovement_Exam : MonoBehaviour
 {
+    private Rigidbody2D rb;
+    private BoxCollider2D coll;
+    private Animator anim;
+
+    [SerializeField] private LayerMask jumpableGround;
+
+    private float dirX = 0f;
+
+
+    [SerializeField]private float moveSpeed = 7f;
+    [SerializeField]private float jumpForce = 40f;
+
+
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        coll = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
+    {
+
+        dirX = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        UpdateAnimationUpdate();
+    }
+
+    private void UpdateAnimationUpdate()
     {
         
+        if (dirX > 0f)
+        {
+            anim.SetBool("Running", true);
+        }
+        else if (dirX < 0)
+        {
+            anim.SetBool("Running", true);
+        }
+        else
+        {
+            anim.SetBool("Running", false);
+        }
     }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
 }
