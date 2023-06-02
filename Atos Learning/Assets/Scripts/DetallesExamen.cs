8111333634348f74a1736de48fbdeb6e2e6a9b0a
a01177702 <a01177702@tec.mx>
2023-06-02 12:26:03 -0600

    Descriptioncontrollerscript

---
+++ Atos Learning/Assets/Scripts/DetallesExamen.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking; 
+using UnityEngine.UI;
+using TMPro;
+using EasyTransition; 
+
+
+
+public class DetallesExamen : MonoBehaviour
+{
+
+    public string actualExam_subject;
+    public string actualExam_dueDate;
+    public string actualExam_title;
+    public string actualExam_description;
+    public string actualExam_image;
+    public string actualExam_questionCount;
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
